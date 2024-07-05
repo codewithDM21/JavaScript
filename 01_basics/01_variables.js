98193892a1f474bffa0f2e38ba4a852c38da99c0
codewithDM21 <deoraj678@gmail.com>
2024-07-05 03:23:45 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 447943
+let accountEmail = "deoraj34@gmail.com"
+var accountPassword = "45678"
+accountCity = "Bokaro"
+
+// accountEmail = "dm@apple.com"no
+// accountId = 447981
+
+console.log(accountIdl);
+
+
+
+
+
+
+
+
+
+
+
+
+'
